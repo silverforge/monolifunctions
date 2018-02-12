6eed120d66995d373ccaf5d7247a828ad2b10c2d
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2018-02-12 18:08:21 +0100

    mail sender added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,22 @@
 import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
 import * as express from 'express';
+import * as _ from 'lodash';
+import * as nodemailer from 'nodemailer';
+
+admin.initializeApp(functions.config().firebase);
 
 const app = express();
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
 
 app.get('/', (request, response) => {
     response.send({
@@ -11,9 +26,29 @@ app.get('/', (request, response) => {
 });
 
 app.get('/detected', (request, response) => {
-    response.send({
-        result: true
-    });
+
+    admin.auth().listUsers()
+        .then(results => {
+            _.forEach(results.users, user => {
+                // console.log(`::: USER MAIL ::: ${user.email}`);
+
+                const mailOptions = {
+                    from: `${gmailEmail}`,
+                    to: user.email,
+                    subject: "motion detected on hodor",
+                    text: "Warning! Motion Detected on hodor!"
+                };
+
+                mailTransport.sendMail(mailOptions).then(() => {
+                    console.warn(`Warning message sent to ${user.email}`);
+                });
+            });
+
+            response.send({
+                result: true
+            });
+        })
+        .catch(err => console.error(err));
 });
 
 function App(req, res) {
